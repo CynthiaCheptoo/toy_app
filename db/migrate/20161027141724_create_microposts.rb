28a4aa255d0cc59af32ae13e3bdbdc642e28aef4
CynthiaCheptoo <cynthiacheptooccm@gmail.com>
2016-10-27 18:04:01 +0300

    initial repository

---
+++ db/migrate/20161027141724_create_microposts.rb
@@ -0,0 +1,10 @@
+class CreateMicroposts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :microposts do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
